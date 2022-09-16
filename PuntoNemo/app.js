745ed066b707bd854727bb01271e24c17909555c
gustavoyanco <gyanco@gmail.com>
2022-09-15 23:35:08 -0300

    estructura para el segundo sprint

---
+++ PuntoNemo/app.js
@@ -0,0 +1,22 @@
+const express = require ('express');
+const path = require ('path');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname,'public')));
+
+app.listen(3200, () => {
+    console.log('Servidor corriendo en puerto 3200')
+});
+app.get ('/',(req,res) => {
+    res.sendFile(path.resolve(__dirname, 'src/views/index.html'));
+});
+app.get ('/register',(req,res) => {
+    res.sendFile(path.resolve(__dirname, 'src/views/register.html'));
+});
+app.get ('/productDetail',(req,res) => {
+    res.sendFile(path.resolve(__dirname, 'src/views/productDetail.html'));
+});
+app.get ('/productCart',(req,res) => {
+    res.sendFile(path.resolve(__dirname, 'src/views/productCart.html'));
+});
